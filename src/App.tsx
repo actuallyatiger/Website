735a91071edb36364655250e46e7c12db3a2b365
actuallyatiger <tigertaylor1024@gmail.com>
2024-06-04 00:55:31 +0100

    Added copyright info and tiles

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,10 @@ import Tile from "./components/Tile";
 
 import pfp from "./assets/pfp.png";
 import insta from "./assets/insta.png";
+import snap from "./assets/snap.png";
+import discord from "./assets/discord.png";
+import threads from "./assets/threads.png";
+import facebook from "./assets/facebook.png";
 
 const App: Component = () => {
   return (
@@ -25,19 +29,25 @@ const App: Component = () => {
           <Tile href="https://instagram.com/actuallyatiger">
             <img src={insta} />
           </Tile>
-          <Tile href="https://instagram.com/actuallyatiger">
-            <img src={insta} />
+          <Tile href="https://snapchat.com/add/possiblyatiger">
+            <img src={snap} />
           </Tile>
-          <Tile href="https://instagram.com/actuallyatiger">
-            <img src={insta} />
+          <Tile href="https://discord.com/users/525078637880410115">
+            <img src={discord} />
           </Tile>
-          <Tile href="https://instagram.com/actuallyatiger">
-            <img src={insta} />
+          <Tile href="https://threads.net/@actuallyatiger">
+            <img src={threads} />
           </Tile>
-          <Tile href="https://instagram.com/actuallyatiger">
-            <img src={insta} />
+          <Tile href="https://facebook.com/possiblyatiger">
+            <img src={facebook} />
           </Tile>
         </Tiles>
+
+        <footer>
+          <p>
+            &copy; All logos are subject to copyright by their respective owners
+          </p>
+        </footer>
       </div>
     </>
   );
