4c71affdcb8fd82327590825d6893760e803c9d0
actuallyatiger <tigertaylor1024@gmail.com>
2024-07-02 23:53:40 +0100

    Add desktop layout, increase image resolution

--- src/components/Tiles.tsx
+++ src/components/Tiles.tsx
@@ -5,7 +5,7 @@ import styles from "../styles/Tiles.module.scss";
 const Tiles: ParentComponent<{ title: string }> = (props) => {
   return (
     <div class={styles.tiles}>
-      <h2>{props.title}</h2>
+      <h2 class={styles.title}>{props.title}</h2>
       <div class={styles.icons}>{props.children}</div>
     </div>
   );
